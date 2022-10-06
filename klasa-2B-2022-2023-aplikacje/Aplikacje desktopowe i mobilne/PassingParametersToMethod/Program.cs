e4b667f812d9377a8a5ceba6975caf6f6fc75092
Michal398 <112950481+Michal398@users.noreply.github.com>
2022-10-06 09:09:41 +0200

    Add files via upload

---
+++ klasa-2B-2022-2023-aplikacje/Aplikacje desktopowe i mobilne/PassingParametersToMethod/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace PassingParametersToMethod
+{
+    class Program
+    {
+        #region
+        static void Main(string[] args)
+        {
+            TestingParamteters testingParamteters = new TestingParamteters();
+            testingParamteters.Test1();
+        }
+        #endregion
+
+
+    }
+}
