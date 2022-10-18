fd3f7002a6d300ac31c4b3c7fba32dd3f36a3c36
Michal398 <112950481+Michal398@users.noreply.github.com>
2022-10-18 13:49:01 +0200

    Add files via upload

--- klasa-2B-2022-2023-aplikacje/Aplikacje desktopowe i mobilne/PassingParametersToMethod/Program.cs
+++ klasa-2B-2022-2023-aplikacje/Aplikacje desktopowe i mobilne/PassingParametersToMethod/Program.cs
@@ -4,14 +4,12 @@ namespace PassingParametersToMethod
 {
     class Program
     {
-        #region
         static void Main(string[] args)
         {
             TestingParamteters testingParamteters = new TestingParamteters();
-            testingParamteters.Test1();
+            //testingParamteters.Test1();
+            //testingParamteters.Test2();
+            testingParamteters.Test3();
         }
-        #endregion
-
-
     }
 }
--- klasa-2B-2022-2023-aplikacje/Aplikacje desktopowe i mobilne/PassingParametersToMethod/TestingParamteters.cs
+++ klasa-2B-2022-2023-aplikacje/Aplikacje desktopowe i mobilne/PassingParametersToMethod/TestingParamteters.cs
@@ -6,30 +6,29 @@ using System.Threading.Tasks;
 
 namespace PassingParametersToMethod
 {
-    #region
-    struct Personstruct
+    struct PersonStruct
     {
-        string name;
-        string surname;
-        int age;
+        public string name;
+        public string surname;
+        public int age;
     }
-    #endregion
 
-{
+
     class TestingParamteters
     {
         #region Przekazywanie typów prostych
+
         public void Test1()
         {
             byte number = 15;
             Console.WriteLine("Zmienna number = " + number);
-            //ChangeValue(number);
+            ChangeValue(number);
             //ChangeValue(6);
-            ChangeValueV2(ref number);
-            //ChangeValueV2(ref 6); BŁAD!!!!!!!!!
+            //ChangeValueV2(ref number);
+            //ChangeValueV2(ref 6); //BŁAD!!!!!!!!!
             Console.WriteLine("Zmienna number = " + number);
 
-            int random = GetRandomNumber();
+            int random;//= GetRandomNumber();
             GetRandomNumber(out random);
         }
 
@@ -58,13 +57,20 @@ namespace PassingParametersToMethod
             value = new Random().Next();
             Console.WriteLine("Parametr value = " + value);
         }
+
         #endregion
 
-        #region przekazywanie typów referencyjnych
+        #region Przekazywanie typów referencyjnych
 
         public void Test2()
         {
             Person person = new Person();
+            person.ShowInfo();
+
+            //GetPerson(person);
+            //GetPerson2(person);
+            GetPerson3(ref person);
+
             person.ShowInfo();
         }
 
@@ -75,14 +81,43 @@ namespace PassingParametersToMethod
             p.ShowInfo();
         }
 
+        private void GetPerson2(Person p)
+        {
+            p = new Person();
+            p.ShowInfo();
+            p.Age = 40;
+            p.ShowInfo();
+        }
+
+        private void GetPerson3(ref Person p)
+        {
+            p = new Person();
+            p.ShowInfo();
+            p.Age = 40;
+            p.ShowInfo();
+        }
+
         #endregion
 
         #region Przekazywanie struktur
+
         public void Test3()
         {
-            Personstruct ps = new Personstruct();
+            PersonStruct ps = new PersonStruct();
             ps.name = "Adam";
+            Console.WriteLine("Pole name w Test3 " + ps.name);
+            GetPresonStruct(ps);
+            Console.WriteLine("Pole name w Test3 " + ps.name);
         }
+
+        private void GetPresonStruct(PersonStruct ps)
+        {
+            Console.WriteLine("Pole name w GetPresonStruct " + ps.name);
+            ps.name = "Ewa";
+            Console.WriteLine("Pole name w GetPresonStruct " + ps.name);
+        }
+
+
         #endregion
     }
 }
