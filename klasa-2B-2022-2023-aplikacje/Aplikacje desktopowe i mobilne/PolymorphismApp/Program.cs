e4d8beaaa32efa33ef4fffeb2ccc2d540a5b7f31
Michal398 <112950481+Michal398@users.noreply.github.com>
2022-10-20 13:18:20 +0200

    Add files via upload

--- klasa-2B-2022-2023-aplikacje/Aplikacje desktopowe i mobilne/PolymorphismApp/Program.cs
+++ klasa-2B-2022-2023-aplikacje/Aplikacje desktopowe i mobilne/PolymorphismApp/Program.cs
@@ -6,7 +6,10 @@ namespace PolymorphismApp
     {
         static void Main(string[] args)
         {
-            
+            dog firstdog = new dog();
+            dog seconddog = new dog("Burek", 5);
+
+            dog thirddog = seconddog;
         }
     }
 }
---
+++ klasa-2B-2022-2023-aplikacje/Aplikacje desktopowe i mobilne/PolymorphismApp/dog.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PolymorphismApp
+{
+    class dog
+    {
+        private string name;
+        private byte age;
+
+        public dog()
+        {
+            name = "Azor";
+            age = 10;
+        }
+
+        public dog(string name, byte age)
+        {
+            this.name = name;
+            this.age = age;
+        }
+
+        public dog(dog dogToCopy)
+        {
+            name = dogToCopy.name;
+            age = dogToCopy.age;
+        }
+        public void ShowInfo()
+        {
+            Console.WriteLine("******************");
+            Console.WriteLine("Pies o imieniu" + name + "ma" + age + "lat");
+            Console.WriteLine("******************");
+        }
+
+        public void Bark()
+        {
+            Console.WriteLine("Pies o imieniu" + name + "szczeka");
+        }
+
+        public void ChasePostman()
+        {
+            Console.WriteLine("pies o imieniu " + age + "goni listonosza");
+        }               
+    }
+}
